765527ce041ec71e173428e919cb9e43fddf9732
SarwatMajeed24 <sarwatmajeed2015@gmail.com>
2024-10-13 16:20:58 +0500

    class 3 Assignment updated

--- assignment/src/app/page.tsx
+++ assignment/src/app/page.tsx
@@ -1,13 +1,11 @@
 
 import Hero from "./components/hero";
-import Link from "next/link";
+
 
 export default function Home() {
   return (
    <div>
     <Hero />
-    <Link href="/about" className="text-white hover:text-gray-300 font-semibold">About Me</Link>
-    <Link href="/contact" className="text-white hover:text-gray-300 font-semibold">Contact</Link>
    </div>
   );
 }
